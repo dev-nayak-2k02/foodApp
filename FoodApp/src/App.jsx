01fe6a9f6d0e3a1c0d42a59400d3bc873d955c10
dev nayak10 <devprasannanayak@gmail.com>
2025-10-29 09:08:37 +0530

    content added

---
+++ FoodApp/src/App.jsx
@@ -0,0 +1,12 @@
+import Search from "./Components/Search"
+
+function App() {
+
+  return (
+    <>
+      <Search/>
+    </>
+  )
+}
+
+export default App
---
+++ FoodApp/src/Components/Search.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Search(){
+    const[query,setQuery] = useState('');
+    return(
+        <div>
+            <input type="text" value={query} onChange={(e)=>setQuery(e.target.value)}/>
+            
+        </div>
+    )
+}
